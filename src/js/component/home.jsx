959cdcc9d482a2fc84c16002dfaa0ea3607f1942
Edvin Johannes Piirsalu <edvin.piirsalu@gmail.com>
2024-11-05 19:07:11 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+	const [inputValue, setInputValue] = useState('');
+	const [task, setTask] = useState([]);
+
+
+	const handleChange = (e) => {
+		console.log("valor actual:", e.target.value);
+		setInputValue(e.target.value);
+	}
+
+	const addTask = (tasks) => {
+		console.log("Tareas recibidas: ", tasks);
+		
+		if (tasks.trim() !== "") {
+			setTask([...task, tasks]);
+			console.log("lista de tareas", [...task, tasks]);
+			
+			setInputValue("")
+		}
+
+	}
+
+		const deleteTask = (indexToDelete) => {
+			setTask(task.filter((_, index) => index !== indexToDelete));
+		}
+
+return(
+	<div className="text-center mt-5">
+		<h2 className="text">To Do List with React</h2>
+		
+		<div className="container mt-3">
+
+			<input
+				className="form-control"
+				placeholder="Add new task"
+				type="text"
+				value={inputValue}
+				onChange={handleChange}
+				onKeyDown={(e) => {
+					if (e.key === "Enter") { addTask(inputValue) }
+				} 
+			} />
+
+	
+
+		<ul className="list-group">
+			{task.map((tasks, index) => (
+				<li className="list-group-item d-flex justify-content-between align-items-center" key={index}>{tasks}<button className="btn btn-light" onClick={() => deleteTask(index)}>x</button></li>
+			) )}
+		</ul>
+
 		</div>
-	);
+	</div>
+);
 };
 
 export default Home;
