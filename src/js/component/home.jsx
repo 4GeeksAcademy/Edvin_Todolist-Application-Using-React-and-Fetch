63349d11ba6833f81f96327e827459a4fa14147c
Edvin Johannes Piirsalu <edvin.piirsalu@gmail.com>
2024-11-07 20:57:54 +0000

    agregando fetch (primeras: POST+ GET || nuevas: PUT DELETE

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -9,7 +9,97 @@ const Home = () => {
 	const [inputValue, setInputValue] = useState('');
 	const [task, setTask] = useState([]);
 
+	/// nueva parte de 
+	
+	useEffect(()=>{
+		crearUsuario()
+	},[])
 
+	const crearUsuario = async() => {
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify()
+		}; 
+		try{
+			const resp = await fetch('https://playground.4geeks.com/todo/users/edvin', options)
+		if(!resp.ok) {
+			throw new Error('error: ', Error)
+		}
+		const data = await resp.json();
+		console.log('Usuaro creado', data)
+		}catch (error) {
+			console.error('error creating the username', error)
+		}
+	}
+
+
+	const recibirTarea = async() => {
+		const options = {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+		};
+		try{
+			const resp = await fetch('https://playground.4geeks.com/todo/users/edvin', options)
+			if(!resp.ok) {
+				throw new Error('error', Error)
+			}
+				const data = resp.json();
+				console.log(data);
+				setTask(data.todos);
+				console.log(setTask);
+			}catch (error){
+				console.error('error bringing the username', error)
+			}
+	}
+
+	const manejarUpdate = async() => {
+		const options = {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify()
+		};
+		try{
+			const resp = await fetch('https://playground.4geeks.com/todo/todos/195', options)
+			if(!resp.ok) {
+				throw new Error('error', Error)
+			}
+			const data = resp.json();
+			console.log(data);
+		}catch (error){
+			console.error('error updating');
+			
+		}
+	}
+
+	const eliminarTarea = async () => {
+		const options = {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+		try{
+			const resp = await fetch('https://playground.4geeks.com/todo/users/edvin', options)
+			if(!resp.ok) {
+				throw new Error('error deleting', Error)
+			}
+		}catch (error) {
+			console.error('error deleting');
+		}
+	}
+
+	useEffect(()=> {
+		recibirTarea();
+	})
+
+ /// hasta aqui 
 	const handleChange = (e) => {
 		console.log("valor actual:", e.target.value);
 		setInputValue(e.target.value);
@@ -33,11 +123,10 @@ const Home = () => {
 
 return(
 	<div className="text-center mt-5">
-		<h2 className="text">To Do List with React</h2>
+		<h2 className="text">To Do List with React and Fetch</h2>
 		
 		<div className="container mt-3">
 
-		<form>
 			<input
 				className="form-control"
 				placeholder="Add new task"
@@ -48,13 +137,18 @@ return(
 					if (e.key === "Enter") { addTask(inputValue) }
 				} 
 			} />
-		</form>
+
 	
+
 		<ul className="list-group">
 			{task.map((tasks, index) => (
-				<li className="list-group-item d-flex justify-content-between align-items-center" key={index}>{tasks}<button className="btn btn-light" onClick={() => deleteTask(index)}>x</button></li>
+				<li className="list-group-item d-flex justify-content-between align-items-center" key={index}>{tasks.label}<button className="btn btn-light" onClick={() => deleteTask(index)}>x</button></li>
 			) )}
 		</ul>
+
+		<p className="mt-3">
+					{task.length} {task.length === 1 ? "task" : "tasks"} in the list
+				</p> 
 
 		</div>
 	</div>
@@ -62,3 +156,4 @@ return(
 };
 
 export default Home;
+
